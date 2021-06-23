735a20d5720b39a50ba945b2480f6dc8813a9972
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-23 08:32:54 -0500

    customer migrations

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -68,6 +68,48 @@ namespace GeneralStore.MVC.Controllers
             return Redirect("Index");
         }
 
+        // GET: Edit
+        // Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
+            Customer customer = _db.Customers.Find(id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        //POST: Edit
+        // Customer/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        public ActionResult Edit(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Details
+        // Customer/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Customer customer = _db.Customers.Find(id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
     }
 }
